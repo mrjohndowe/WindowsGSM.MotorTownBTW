bb079c6e8b80ed198e8bfbb10d29e395061f15a8
TheRealSarcasmO <31523694+TheRealSarcasmO@users.noreply.github.com>
2025-01-11 16:19:13 -0500

    Update MotorTownBTW.cs
    meh...

--- MotorTownBTW.cs
+++ MotorTownBTW.cs
@@ -15,22 +15,29 @@ namespace WindowsGSM.Plugins
             name = "WindowsGSM.MotorTownBTW",
             author = "TheRealSarcasmO",
             description = "WindowsGSM plugin for supporting MotorTownBTW Dedicated Server",
-            version = "2025.01.11.1540", // format "YYYY.MM.DD.HHMM"
+            version = "2025.01.11.1551", // format "YYYY.MM.DD.HHMM"
             url = "https://github.com/TheRealSarcasmO/WindowsGSM.MotorTownBTW",
             color = "#34FFeb"
         };
 
         // - Standard Constructor and properties
-        public MotorTownBTW(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        public MotorTownBTW(ServerConfig serverData) : base(serverData)
+        {
+            base.serverData = _serverData = serverData;
+            AppIdnParam = $"{AppId} {customParam}";
+        }
         private readonly ServerConfig _serverData;
         public string ErrorMessage, NoticeMessage;
 
         // - Game server Fixed variables
-        public override string StartPath => "\\steamapps\\common\\Motor Town Behind The Wheel - Dedicated Server\\MotorTown\\Binaries\\Win64\\MotorTownServer-Win64-Shipping.exe";
+        public override string StartPath => "MotorTown\\Binaries\\Win64\\MotorTownServer-Win64-Shipping.exe";
         public string FullName = "MotorTownBTW Dedicated Server";
         public bool AllowsEmbedConsole = true;
         public int PortIncrements = 3;
         public object QueryMethod = new A2S(); // Query method should be A2S for this game server
+
+        //web interface info TODO
+        public string WebAPIPort = "27014";
 
         // - Game server default values
         public string Port = "27015";
@@ -39,17 +46,16 @@ namespace WindowsGSM.Plugins
         public string Maxplayers = "200";
         public string Additional = "Jeju_World?listen? -server -log -useperfthreads";
 
-        //web interface info TODO
-        public string WebAPIPort = "8080";
-
         // - Settings properties for SteamCMD installer
         public override bool loginAnonymous => false; // Click Login and add your login info to txt file keep this safe.
         public string AppId = "2223650";
+        public string customParam = " -beta test -betapassword motortowndedi";
+        public string AppIdnParam;
 
         public async Task<Process> Install()
         {
             var steamCMD = new Installer.SteamCMD();
-            Process p = await steamCMD.Install(_serverData.ServerID, string.Empty, AppId);
+            Process p = await steamCMD.Install(_serverData.ServerID, string.Empty, AppIdnParam);
             Debug.WriteLine(steamCMD.Error);
 
             return p;
@@ -75,7 +81,7 @@ namespace WindowsGSM.Plugins
         {
             return null;
         }
-        
+
         // - Stop server function
         public async Task Stop(Process p)
         {
