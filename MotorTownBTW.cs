9476ee3f0735dedbe2daa7405e0d57bc28347523
TheRealSarcasmO <31523694+TheRealSarcasmO@users.noreply.github.com>
2025-01-11 19:02:36 -0500

    Update MotorTownBTW.cs
    adjusted how server setting are set during install, also updated version.

--- MotorTownBTW.cs
+++ MotorTownBTW.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using WindowsGSM.Functions;
 using WindowsGSM.GameServer.Engine;
@@ -15,24 +17,24 @@ namespace WindowsGSM.Plugins
             name = "WindowsGSM.MotorTownBTW",
             author = "TheRealSarcasmO",
             description = "WindowsGSM plugin for supporting MotorTownBTW Dedicated Server",
-            version = "2025.01.11.1625", // format "YYYY.MM.DD.HHMM"
+            version = "2025.01.11.1919-alpha", // format "YYYY.MM.DD.HHMM"
             url = "https://github.com/TheRealSarcasmO/WindowsGSM.MotorTownBTW",
             color = "#34FFeb"
         };
 
         // - Standard Constructor and properties
-        public MotorTownBTW(ServerConfig serverData) : base(serverData)
-        {
-            base.serverData = _serverData = serverData;
-            AppIdnParam = $"{AppId} {customParam}";
-        }
-        private readonly ServerConfig _serverData;
-        public string ErrorMessage, NoticeMessage;
+        public MotorTownBTW(ServerConfig serverData) : base(serverData) => base.serverData = serverData;
+        
+        // {
+        // AppIdnParam = $"{AppId} {customParam}";
+        // }
+        // private readonly ServerConfig _serverData;
+        // public string ErrorMessage, NoticeMessage;
 
         // - Game server Fixed variables
         public override string StartPath => "MotorTown\\Binaries\\Win64\\MotorTownServer-Win64-Shipping.exe";
         public string FullName = "MotorTownBTW Dedicated Server";
-        public bool AllowsEmbedConsole = true;
+        public bool AllowsEmbedConsole = true; // Allowed but Game currently does not support it as in doesn't let you use server console only here for info.
         public int PortIncrements = 3;
         public object QueryMethod = new A2S(); // Query method should be A2S for this game server
 
@@ -42,32 +44,32 @@ namespace WindowsGSM.Plugins
         // - Game server default values
         public string Port = "27015";
         public string QueryPort = "27016";
-        public string Defaultmap = "jeju_world";
+        public string Defaultmap = "Jeju_world";
         public string Maxplayers = "200";
-        public string Additional = "Jeju_World?listen? -server -log -useperfthreads";
+        public string Additional = "-log -useperfthreads";
 
         // - Settings properties for SteamCMD installer
         public override bool loginAnonymous => false; // Click Login and add your login info to txt file keep this safe.
         public override string AppId => "2223650 -beta test -betapassword motortowndedi";
-     //   public string customParam = " -beta test -betapassword motortowndedi";
-     //   public string AppIdnParam;
+        //   public string customParam = " -beta test -betapassword motortowndedi";
+        //   public string AppIdnParam;
 
-//        public async Task<Process> Install()
- //       {
-  //          var steamCMD = new Installer.SteamCMD();
-   //         Process p = await steamCMD.Install(_serverData.ServerID, string.Empty, AppIdnParam, true, loginAnonymous);  // Thanks for reminding me Raziel7893
-    //        Debug.WriteLine(steamCMD.Error);
-//
- //           return p;
-  //      }
+        //        public async Task<Process> Install()
+        //       {
+        //          var steamCMD = new Installer.SteamCMD();
+        //         Process p = await steamCMD.Install(_serverData.ServerID, string.Empty, AppIdnParam, true, loginAnonymous);  // Thanks for reminding me Raziel7893
+        //        Debug.WriteLine(steamCMD.Error);
+        //
+        //           return p;
+        //      }
 
-//        public async Task<Process> Update(bool validate = true, string custom = null)
-  //      {
-    //        if (custom == null)
-      //          custom = " -beta test -betapassword motortowndedi";
+        //        public async Task<Process> Update(bool validate = true, string custom = null)
+        //      {
+        //        if (custom == null)
+        //          custom = " -beta test -betapassword motortowndedi";
         //    var (p, error) = await Installer.SteamCMD.UpdateEx(_serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);  // Thanks for reminding me Raziel7893
-          //  Debug.WriteLine(error);
-            //return p;
+        //  Debug.WriteLine(error);
+        //return p;
         //}
 
         // - Create a default cfg for the game server after installation
@@ -77,9 +79,69 @@ namespace WindowsGSM.Plugins
         }
 
         // - Start server function, return its Process to WindowsGSM
+        // - Start server function, return its Process to WindowsGSM
         public async Task<Process> Start()
         {
-            return null;
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, StartPath);
+            if (!File.Exists(shipExePath))
+            {
+                Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                return null;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{serverData.ServerMap}?listen -server ");
+            sb.Append($"{serverData.ServerParam} ");
+            //sb.Append($" -MultiHome={_serverData.ServerIP}"); //maybe change to online IP
+            //sb.Append($" -Port={_serverData.ServerPort} ");
+            //sb.Append($" -QueryPort={_serverData.ServerQueryPort} ");
+            //
+
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    CreateNoWindow = false,
+                    WorkingDirectory = ServerPath.GetServersServerFiles(serverData.ServerID),
+                    FileName = shipExePath,
+                    Arguments = sb.ToString(),
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    UseShellExecute = false
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (serverData.EmbedConsole)
+            {
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                p.StartInfo.CreateNoWindow = true;
+                var serverConsole = new ServerConsole(serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+                if (serverData.EmbedConsole)
+                {
+                    p.BeginOutputReadLine();
+                    p.BeginErrorReadLine();
+                }
+                return p;
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+                return null; // return null if fail to start
+            }
         }
 
         // - Stop server function
@@ -89,6 +151,10 @@ namespace WindowsGSM.Plugins
             {
                 Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
                 Functions.ServerConsole.SendWaitToMainWindow("^c");                 //Trying to grace-fully exit
+                p.WaitForExit(1000)
+                Functions.ServerConsole.SendWaitToMainWindow("^c");
+                p.WaitForExit(1000)
+                Functions.ServerConsole.SendWaitToMainWindow("^c");
                 p.WaitForExit(20000);
             });
         }
